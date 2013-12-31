7d9f496964fba9a4b906cb5f3382d7f56ec30b5b
liam-kelly <liam@bluetent.com>
2013-12-30 19:38:13 -0700

    Started adding functions to pages class.

--- includes/models/pages.php
+++ includes/models/pages.php
@@ -8,6 +8,27 @@
 
 class pages{
 
-    
+    //Define class properties
+    public $page_id;
+    public $name;
+    public $path;
+
+    //Constructor
+    public function __construct(){
+
+
+
+    }
+
+    //Lookup a page
+    public function lookup($name = 'home'){
+
+    }
+
+    //Add a page
+
+    //Change a page
+
+    //Delete a page
 
 }
