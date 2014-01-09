67c900ba2736fc23ba5684c8dca066ff625740bc
liam-kelly <liam@bluetent.com>
2014-01-08 17:58:27 -0700

    Updated copyright notices to reflect the creation date.

--- includes/views/themes/default/login.php
+++ includes/views/themes/default/login.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2013 William Caleb Kelly
+ * Copyright 2014 William Caleb Kelly
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
